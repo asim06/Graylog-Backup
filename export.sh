42f6707158c47afe6d703fb995c89e6ce452e1e5
Asım MISIRLI <asimmisirli@hotmail.com>
2020-02-22 16:09:44 +0300

    Update export.sh

--- export.sh
+++ export.sh
@@ -10,7 +10,7 @@ fi
 
 echo "Starting $ADAYAGO"
 QUERY_STR='{"query":{"range":{"timestamp":{"gte":"'"$ADAYAGO "'00:00:00.000","lte":"'"$ADAYAGO"' 23:59:59.999"}}}}'
-EXPORT_PATH=/logs/exports/elasticsearch/
+EXPORT_PATH=elasticsearch/
 EXPORT_FILE_NAME=esbackup.$ADAYAGO.json
 es-export-bulk --url http://localhost:9200 --file $EXPORT_PATH/$EXPORT_FILE_NAME --body "$QUERY_STR"
 ls -lh $EXPORT_PATH/$EXPORT_FILE_NAME
@@ -20,7 +20,7 @@ sha1sum $EXPORT_PATH/$EXPORT_FILE_NAME.gz >> $EXPORT_PATH/$EXPORT_FILE_NAME.gz.h
 sha256sum $EXPORT_PATH/$EXPORT_FILE_NAME.gz >> $EXPORT_PATH/$EXPORT_FILE_NAME.gz.hash
 
 cd $EXPORT_PATH
-/usr/bin/java -jar /usr/local/sbin/tss-client-console-3.1.5.jar -Z $EXPORT_FILE_NAME.gz.hash http://zd.kamusm.gov.tr 80 19006 SIFREYI_GONDERECEGIM &
+/usr/bin/java -jar /usr/local/sbin/tss-client-console-3.1.5.jar -Z $EXPORT_FILE_NAME.gz.hash http://zd.kamusm.gov.tr 80 19006 Şifre &
 /usr/bin/java -jar /usr/local/sbin/tss-client-console-3.1.5.jar -K http://zd.kamusm.gov.tr 80 19006 Şifre >> /logs/exports/kalan_kredi
 cd -
 
