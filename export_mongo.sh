508fd6c72703e4589fc5cc24abcfabd2022f7bf4
Asım MISIRLI <asimmisirli@hotmail.com>
2020-02-22 16:04:45 +0300

    Update export_mongo.sh

--- export_mongo.sh
+++ export_mongo.sh
@@ -1,7 +1,7 @@
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 
 DIR=`date +%m%d%y`
-DEST=/logs/exports/mongodb/graylog_mongodb_$DIR
+DEST=mongodb/graylog_mongodb_$DIR
 mkdir $DEST
 mongodump -o $DEST
 tar czf /logs/exports/mongodb/graylog_mongodb_$DIR.tgz $DEST
